2f31430b94df8a6759d36f4ced7fd8cac53bba97
Rob Forman <rob@robforman.com>
2012-12-03 10:33:39 -0800

    send to mailer by name lookup

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,12 @@
 class EmailsController < ApplicationController
   def create
-    email = TestMailer.test_template(params).deliver
+    mailer = params.fetch(:mailer).camelcase.constantize
+    template = params.fetch(:template).to_sym
+    Rails.logger.info("Sending #{mailer}.#{template}")
+
+    # TODO: check if this mailer and template exist
+
+    email = mailer.public_send(template, params).deliver
     render nothing: true, success: true
   end
 end
