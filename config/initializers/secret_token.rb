71b373310507bf5f399ab2de52f986b20704653a
Rob Forman <rob@robforman.com>
2012-12-03 07:48:00 -0800

    project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailercity::Application.config.secret_token = '0e418cd4d754cf9ffbb879c6f55cdd0f04f8ff3348314ed98db59a2623a11da7017c200ebb4264a546664b123529221ac4aabdde48defbea229035dfe011835a'
